9bed2e91f0fdb210f8bf02d4c1422a001a13c152
Eman-Bakri <eman1996-sy@hotmail.com>
2024-04-09 02:24:32 +0000

    update

--- 100-createIteratorObject.js
+++
@@ -1,7 +0,0 @@
-export default function createIteratorObject(report) {
-  const employes = [];
-  for (const departmentEmployees of Object.values(report.allEmployees)) {
-    employes.push(...departmentEmployees);
-  }
-  return employes;
-}
